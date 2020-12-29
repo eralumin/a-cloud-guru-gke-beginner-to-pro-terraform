b8b16440d5e349d11763908256e622f8529901e9
Kevin Marilleau <kevin.marilleau@gmail.com>
2020-12-29 01:14:02 +0100

    chapter_2_2 remove custom node pool

--- chapter_2/2_creating_a_gke_cluster/main.tf
+++ chapter_2/2_creating_a_gke_cluster/main.tf
@@ -2,25 +2,10 @@ resource "google_container_cluster" "primary" {
   name     = "${var.lab_name}-cluster"
   location = var.region
 
-  remove_default_node_pool = true
-  initial_node_count       = 1
+  initial_node_count = 1
 
   provisioner "local-exec" {
     command = "gcloud container clusters get-credentials ${google_container_cluster.primary.name} --region ${var.region} --project ${var.project}"
-  }
-}
-
-resource "google_container_node_pool" "primary_preemptible_nodes" {
-  name       = "${var.lab_name}-node-pool"
-  location   = var.region
-  cluster    = google_container_cluster.primary.name
-  node_count = 1
-
-  node_config {
-    preemptible  = true
-    machine_type = "g1-small"
-
-    oauth_scopes = ["https://www.googleapis.com/auth/cloud-platform"]
   }
 }
 
